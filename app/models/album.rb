5cf0190ac63846d46550ea98041073381b25f55e
Andrew T. poe <techapoe@gmail.com>
2015-07-09 09:59:22 -0400

    Some minor updates after in-class review

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,7 +1,6 @@
 class Album < ActiveRecord::Base
   validates :title, :genre, :artist, :length, presence: true
-  validates :title, :genre, :artist, :length, length: { minimum: 1 }
   validates :length, numericality: true
-  validates :title, uniqueness: {scope: :artist}
+  validates :title, uniqueness: { scope: :artist }
 
 end
--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
 
   test "Ideal conditions are valid" do
     a = Album.new(title: 'test_title', genre: 'test_genre', artist: 'test_artist', length: 100)
@@ -11,71 +8,65 @@ class AlbumTest < ActiveSupport::TestCase
   end
 
   test "Valid record can be saved and retrieved" do
-  # Make record
-  a = Album.new(title: 'test_title', genre: 'test_genre', artist: 'test_artist', length: 100)
-  a.save
-  # Validate record is present
-  record = Album.find_by(title: 'test_title')
-  assert record
+    Album.create!(title: 'test_title', genre: 'test_genre', artist: 'test_artist', length: 100)
+    record = Album.find_by(title: 'test_title')
+    assert record
   end
 
   test "Title must be present" do
     a = Album.new(genre: 'test_genre', artist: 'test_artist', length: 100)
-    assert_equal a.valid?, false
+    refute a.valid?
   end
 
   test "Genre must be present" do
     a = Album.new(title: 'test_title', artist: 'test_artist', length: 100)
-    assert_equal a.valid?, false
+    refute a.valid?
   end
 
   test "Artist must be present" do
     a = Album.new(title: 'test_title', genre: 'test_genre', length: 100)
-    assert_equal a.valid?, false
+    refute a.valid?
   end
 
   test "Length must be present" do
     a = Album.new(title: 'test_title', genre: 'test_genre', artist: 'test_artist')
-    assert_equal a.valid?, false
+    refute a.valid?
   end
 
   test "Title can not have length of zero" do
     a = Album.new(title: '', genre: 'test_genre', artist: 'test_artist', length: 100)
-    assert_equal a.valid?, false
+    refute a.valid?
   end
 
   test "Genre can not have length of zero" do
     a = Album.new(title: 'test_title', genre: '', artist: 'test_artist', length: 100)
-    assert_equal a.valid?, false
+    refute a.valid?
   end
 
   test "Artist can not have length of zero" do
     a = Album.new(title: 'test_title', genre: 'test_genre', artist: '', length: 100)
-    assert_equal a.valid?, false
+    refute a.valid?
   end
 
   test "Length must be an integer" do
     a = Album.new(title: 'test_title', genre: 'test_genre', artist: 'test_artist', length: 'fff')
-    assert_equal a.valid?, false
+    refute a.valid?
   end
 
   test "Artist & Song can not be duplicated" do
-    a = Album.new(title: 'test_title', genre: 'test_genre', artist: 'test_artist', length: 100)
-    a.save
+    Album.create!(title: 'test_title', genre: 'test_genre', artist: 'test_artist', length: 100)
     b = Album.new(title: 'test_title', genre: 'test_genre2', artist: 'test_artist', length: 100)
-    assert_equal b.valid?, false
+    refute b.valid?
   end
 
   test "Artist can have multiple songs" do
-    a = Album.new(title: 'test_title', genre: 'test_genre', artist: 'test_artist', length: 100)
-    a.save
+    Album.create!(title: 'test_title', genre: 'test_genre', artist: 'test_artist', length: 100)
     b = Album.new(title: 'second_test_title', genre: 'test_genre2', artist: 'test_artist', length: 100)
     assert b.valid?
   end
 
   test "Multiple artists can have same song title" do
-    a = Album.new(title: 'test_title', genre: 'test_genre', artist: 'test_artist', length: 100)
-    a.save
+    Album.create!(title: 'test_title', genre: 'test_genre', artist: 'test_artist', length: 100)
     b = Album.new(title: 'test_title', genre: 'test_genre', artist: 'second_test_artist', length: 100)
     assert b.valid?
   end
